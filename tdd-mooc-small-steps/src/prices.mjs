a0975a5a1bc19013a417fd85aff90a6ad81dee63
jloiz <local.dev@mail.com>
2026-07-19 21:19:57 +0100

    small steps: remove dead code and rename variables

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -18,18 +18,10 @@ function createApp(database) {
     const age = req.query.age ? parseInt(req.query.age) : undefined;
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
-    const date = parseDate(req.query.date);
-    const date2 = parsePlainDate(req.query.date);
-    const cost = calculateCost(age, type, date2, baseCost);
+    const date = parsePlainDate(req.query.date);
+    const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
-
-  function parseDate(dateString) {
-    if (dateString) {
-      Temporal.PlainDate.from(dateString)
-      return new Date(dateString);
-    }
-  }
 
     function parsePlainDate(dateString) {
     if (dateString) {
