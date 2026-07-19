15bbc5d67214d898ec2917209856128b07bba3c8
jloiz <local.dev@mail.com>
2026-07-19 18:53:40 +0100

    small steps: add new date to isHoliday signature

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -78,7 +78,7 @@ function createApp(database) {
 
   function calculateReduction(date, date2) {
     let reduction = 0;
-    if (date2 && isMonday(date2) && !isHoliday(date)) {
+    if (date2 && isMonday(date2) && !isHoliday(date, date2)) {
       reduction = 35;
     }
     return reduction;
@@ -88,7 +88,7 @@ function createApp(database) {
     return date2.dayOfWeek === 1;
   }
 
-  function isHoliday(date) {
+  function isHoliday(date, date2) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
