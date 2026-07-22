9bd41b49932e9303340dc9029f3253ceee6664c0
jloiz <local.dev@mail.com>
2026-07-22 20:15:18 +0100

    tetris: level-1, revert as broke rules of changing only if needed

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -29,9 +29,6 @@ export class Board {
     this.isFalling = true
   }
 
-  isShapesFirstTick(count) {
-    return (count%3 === 1) ? true : false
-  }
 
   drop(shape) {
     if (this.currentShape === "NO_SHAPE") {
@@ -50,19 +47,13 @@ export class Board {
   tick() {
     // TODO: REFACTOR THIS DEPSERATELY
     this.tickCount++
-    console.log("tickCount:", this.tickCount, this.isShapesFirstTick(this.tickCount))
+    console.log("tickCount:", this.tickCount, "tickCount%3", this.tickCount % 3)
     // ToDo: Maybe loop on new shape
-    if (this.isShapesFirstTick(this.tickCount)) {
+    if (this.tickCount === 1) {
       this.row1 = "..."
       this.row2 = `.${this.currentShape}.`
       this.drawBoard()
-    } 
-    // if (this.tickCount%3 === 1) {
-    //   this.row1 = "..."
-    //   this.row2 = `.${this.currentShape}.`
-    //   this.drawBoard()
-    // }
-    if (this.tickCount === 2) {
+    } if (this.tickCount === 2) {
       this.row2 = "..."
       this.row3 = `.${this.currentShape}.`
       this.clearShape()
@@ -70,7 +61,11 @@ export class Board {
     } if (this.tickCount === 3) {
       this.isFalling = false
       this.drawBoard()
-    }  if (this.tickCount === 5) {
+    } if (this.tickCount === 4) {
+      this.row1 = "..."
+      this.row2 = `.${this.currentShape}.`
+      this.drawBoard()
+    } if (this.tickCount === 5) {
       this.isFalling = false
       this.drawBoard()
     }
