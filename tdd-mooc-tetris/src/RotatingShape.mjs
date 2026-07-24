9a81b0132281d8887c38f4b1cd422b18e39e7d29
jloiz <local.dev@mail.com>
2026-07-24 20:27:30 +0100

    tetris: level-2, implement dynamic anticlockwise rotation

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -11,27 +11,27 @@ export class RotatingShape {
         return new RotatingShape(formattedShape)
     }
 
-    rotateRight() {   
-        const cleanShape = this.#shape.substring(0, this.#shape.length -1)
+    rotateRight() {
+        const cleanShape = this.#shape.substring(0, this.#shape.length - 1)
         const rows = cleanShape.split('\n')
-        const shapeMatrix = rows.map(row => row.split('')) 
+        const shapeMatrix = rows.map(row => row.split(''))
         const shapeMatrixHeight = shapeMatrix.length
         const shapeMatrixLength = shapeMatrix[0].length
         const newShapeMatrix = structuredClone(shapeMatrix)
         // Algorithm to rotate a matrix clockwise is to transpose and reverse rows
 
-        // Transpse matrix
-        for (let i=0; i < shapeMatrixHeight; i++){
-            for(let j=0; j < shapeMatrixLength; j++){
+        // Transpose matrix
+        for (let i = 0; i < shapeMatrixHeight; i++) {
+            for (let j = 0; j < shapeMatrixLength; j++) {
                 newShapeMatrix[i][j] = shapeMatrix[j][i]
             }
         }
 
         // Reverse rows
-        const rotatedShapeMatrix = newShapeMatrix.map(row => {return row.reverse()}) 
-        
+        const rotatedShapeMatrix = newShapeMatrix.map(row => { return row.reverse() })
+
         // Rejoin elements into row
-        const rotatedShapeRows = rotatedShapeMatrix.map(row => {return row.join('')})
+        const rotatedShapeRows = rotatedShapeMatrix.map(row => { return row.join('') })
 
         let rotatedShape = rotatedShapeRows.join('\n')
         rotatedShape = `${rotatedShape}\n`
@@ -40,12 +40,30 @@ export class RotatingShape {
     }
 
     rotateLeft() {
-        var hardcoded =
-            `CFI
-       BEH
-       ADG`
+        const cleanShape = this.#shape.substring(0, this.#shape.length - 1)
+        const rows = cleanShape.split('\n')
+        const shapeMatrix = rows.map(row => row.split(''))
+        const shapeMatrixHeight = shapeMatrix.length
+        const shapeMatrixLength = shapeMatrix[0].length
+        const newShapeMatrix = structuredClone(shapeMatrix)
+        // Algorithm to rotate a matrix clockwise is to transpose and reverse columns
 
-        var formattedShape = `${hardcoded.replaceAll(' ', '')}\n`
+        // Transpose matrix
+        for (let i = 0; i < shapeMatrixHeight; i++) {
+            for (let j = 0; j < shapeMatrixLength; j++) {
+                newShapeMatrix[i][j] = shapeMatrix[j][i]
+            }
+        }
+        console.log("HERE")
+        console.log(JSON.stringify(newShapeMatrix))
+        
+        // Reverse columns
+        var newShapeRows = newShapeMatrix.map(row => {return row.join('')})
+        var rotatedShapeMatrix = newShapeRows.reverse()
+        console.log(JSON.stringify(rotatedShapeMatrix))
+
+
+        var formattedShape = `${rotatedShapeMatrix.join('\n')}\n`
         return new RotatingShape(formattedShape)
     }
 
