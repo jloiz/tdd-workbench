1dc86e5e9d90b544cb0855ee466005fe9968be2d
jloiz <local.dev@mail.com>
2026-07-24 19:23:27 +0100

    tetris: level-2, implement a verbose clockwise rotation algorithm

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -11,17 +11,27 @@ export class RotatingShape {
         return new RotatingShape(formattedShape)
     }
 
-    rotateRight() {
-        var hardcoded =
-            `GDA\nHEB\nIFC\n`
-        // var rows = this.#shape.split('\n')    
-        var rows = hardcoded.split('\n')
-        
-        var elements = rows.map(row => row.split(''))
+    rotateRight() {   
+        var cleanShape = this.#shape.substring(0, this.#shape.length -1)
+        var rows = cleanShape.split('\n')
+        var shapeMatrix = rows.map(row => row.split(''))       
 
-        console.log(elements)
+        var newElements = structuredClone(shapeMatrix)
+        // Algorithm to rotate a matrix clockwise is to transpose and reverse rows
 
-        var newRows = rows.join('\n')
+        // Transpse matrix
+        for (let i=0; i < 3; i++){
+            for(let j=0; j < 3; j++){
+                newElements[i][j] = shapeMatrix[j][i]
+            }
+        }
+
+        // Reverse rows
+        var newElements2 = newElements.map(row => {return row.reverse()}) 
+        newElements2 = newElements2.map(row => {return row.join('')})
+
+        var newRows = newElements2.join('\n')
+        newRows = `${newRows}\n`
 
         return new RotatingShape(newRows)
     }
