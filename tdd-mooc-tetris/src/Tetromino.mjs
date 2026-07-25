acfa930dc20885e5a3b35e6b646e85378a75cddb
jloiz <local.dev@mail.com>
2026-07-25 19:26:13 +0100

    tetris: level-3, start introducing Tetromino constructor

--- tdd-mooc-tetris/src/Tetromino.mjs
+++ tdd-mooc-tetris/src/Tetromino.mjs
@@ -2,13 +2,20 @@ import { RotatingShape } from "./RotatingShape.mjs"
 
 export class Tetromino {
     
-    tetromino
+    tetromino;
+    tetrominoState
+
+    constructor(initialState){
+        this.tetromino;
+        this.tetrominoState = initialState
+    }
 
     static T_SHAPE = Tetromino.fromString('.T.\nTTT\n...\n')
     static I_SHAPE = Tetromino.fromString('.....\n.....\nIIII.\n.....\n.....\n')
 
     static fromString(tetrominoString) {
         this.tetromino = tetrominoString;
+        test = new Tetromino(tetrominoString)
         return new RotatingShape(tetrominoString)
     } 
 
