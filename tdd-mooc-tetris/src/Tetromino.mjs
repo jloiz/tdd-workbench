0a9ffbb60b1a4f91059669fec0c2bc3370879e8b
jloiz <local.dev@mail.com>
2026-07-25 19:22:26 +0100

    tetris: level-3, implemtnt I shape

--- tdd-mooc-tetris/src/Tetromino.mjs
+++ tdd-mooc-tetris/src/Tetromino.mjs
@@ -5,6 +5,7 @@ export class Tetromino {
     tetromino
 
     static T_SHAPE = Tetromino.fromString('.T.\nTTT\n...\n')
+    static I_SHAPE = Tetromino.fromString('.....\n.....\nIIII.\n.....\n.....\n')
 
     static fromString(tetrominoString) {
         this.tetromino = tetrominoString;
