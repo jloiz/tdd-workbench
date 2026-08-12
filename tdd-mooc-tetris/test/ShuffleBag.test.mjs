dfcc04b83ce6bbb8a3493b013697d287853acb33
jloiz <local.dev@mail.com>
2026-08-12 17:03:52 +0100

    tetris: level-10, add red on getting all items of the array

--- tdd-mooc-tetris/test/ShuffleBag.test.mjs
+++ tdd-mooc-tetris/test/ShuffleBag.test.mjs
@@ -4,27 +4,32 @@ import { ShuffleBag } from "../src/ShuffleBag.mjs"
 
 describe("Shuffling the bag", () => {
     let testItems;
+    let bag;
     beforeEach(() => {
         testItems = [1, 2, {}, [], "egg"]
+        bag = new ShuffleBag()
     })
 
     test("it can create a bag", () => {
-        let bag;
-        bag = new ShuffleBag()
         expect(bag).to.exist
     })
 
     test("it can have items added to it", () => {
-        let bag;
-        bag = new ShuffleBag()
         bag.fill(testItems)
         expect(bag.contents).to.equal(testItems)
     })
 
     test("it gives an individual item, that is part of the contents", () => {
-        let bag;
-        bag = new ShuffleBag()
         bag.fill(testItems)
         expect(bag.pull()).to.be.oneOf(testItems)
+    })
+
+    test("it pulls all items from the bag", ()=> {
+        let pulledItems = []
+        bag.fill(testItems)
+        for (let i=0; i< pulledItems.length; i++){
+            pulledItems.push(bag.pull)
+        }
+        expect(pulledItems).toEqual(expect.arrayContaining(testItems))
     })
 })
