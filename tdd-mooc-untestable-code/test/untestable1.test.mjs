b0325bfea78e48751c9a6db4e703aa06bcee6113
jloiz <private@mail.com>
2026-08-24 20:24:37 +0100

    untestable code: untestable1, red on calling the current year with a better date value closer to Date.now()

--- tdd-mooc-untestable-code/test/untestable1.test.mjs
+++ tdd-mooc-untestable-code/test/untestable1.test.mjs
@@ -1,4 +1,4 @@
-import { describe, test } from "vitest";
+import { describe, test, vi } from "vitest";
 import { expect } from "chai";
 import { daysUntilChristmas } from "../src/untestable1.mjs";
 
@@ -8,9 +8,15 @@ describe("Untestable 1: days until Christmas", () => {
   });
 
   test("it accepts a current date", () => {
-    let testCurrentDate = new Date(2026, 8, 24)
-
-    expect(daysUntilChristmas(testCurrentDate)).to.be.a("number")
+    let currentDate = new Date("2026-08-24T03:24:00")
+    expect(daysUntilChristmas(currentDate)).to.be.a("number")
   });
+
+  test("it uses the current year", () => {
+    let currentDate = new Date("2026-08-24T03:24:00")
+    let spy = vi.spyOn(currentDate, "getFullYear")
+
+    expect(spy).toHaveBeenCalled()
+  })
 
 });
