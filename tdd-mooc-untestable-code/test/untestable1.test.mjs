58c4eb2a10780831758ecc5a6d8457e169bc7536
jloiz <private@mail.com>
2026-08-25 17:12:03 +0100

    untestable code: untestable1, red on getDay

--- tdd-mooc-untestable-code/test/untestable1.test.mjs
+++ tdd-mooc-untestable-code/test/untestable1.test.mjs
@@ -5,23 +5,29 @@ import { daysUntilChristmas } from "../src/untestable1.mjs";
 describe("Untestable 1: days until Christmas", () => {
   let currentDate;
   beforeEach(() => {
-    currentDate = new Date("2026-08-24T03:24:00:000")
-  })
+    currentDate = new Date("2026-08-24T03:24:00:000");
+  });
 
   test("it accepts a current date", () => {
-    expect(daysUntilChristmas(currentDate)).to.be.a("number")
+    expect(daysUntilChristmas(currentDate)).to.be.a("number");
   });
 
   test("it uses the current year", () => {
-    let spy = vi.spyOn(currentDate, "getFullYear")
-    daysUntilChristmas(currentDate)
-    expect(spy).toHaveBeenCalled()
-  })
+    let spy = vi.spyOn(currentDate, "getFullYear");
+    daysUntilChristmas(currentDate);
+    expect(spy).toHaveBeenCalled();
+  });
 
   test("it uses the current month", () => {
-    let spy = vi.spyOn(currentDate, "getMonth")
-    daysUntilChristmas(currentDate)
-    expect(spy).toHaveBeenCalled()
-  })
+    let spy = vi.spyOn(currentDate, "getMonth");
+    daysUntilChristmas(currentDate);
+    expect(spy).toHaveBeenCalled();
+  });
 
+  test("it uses the current day", () => {
+    let currentDate = new Date("2026-08-24T03:24:00:000");
+    let spy = vi.spyOn(currentDate, "getDay");
+    daysUntilChristmas(currentDate);
+    expect(spy).toHaveBeenCalled();
+  });
 });
