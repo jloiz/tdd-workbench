8e2088e631ed68e8288f6e76360ffc86e913401b
jloiz <private@mail.com>
2026-08-25 17:10:26 +0100

    untestable code: untestable1, red on get month

--- tdd-mooc-untestable-code/test/untestable1.test.mjs
+++ tdd-mooc-untestable-code/test/untestable1.test.mjs
@@ -1,17 +1,25 @@
-import { describe, test, vi } from "vitest";
+import { beforeEach, describe, test, vi } from "vitest";
 import { expect } from "chai";
 import { daysUntilChristmas } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
+  let currentDate;
+  beforeEach(() => {
+    currentDate = new Date("2026-08-24T03:24:00:000")
+  })
 
   test("it accepts a current date", () => {
-    let currentDate = new Date("2026-08-24T03:24:00:000")
     expect(daysUntilChristmas(currentDate)).to.be.a("number")
   });
 
   test("it uses the current year", () => {
-    let currentDate = new Date("2026-08-24T03:24:00:000")
     let spy = vi.spyOn(currentDate, "getFullYear")
+    daysUntilChristmas(currentDate)
+    expect(spy).toHaveBeenCalled()
+  })
+
+  test("it uses the current month", () => {
+    let spy = vi.spyOn(currentDate, "getMonth")
     daysUntilChristmas(currentDate)
     expect(spy).toHaveBeenCalled()
   })
