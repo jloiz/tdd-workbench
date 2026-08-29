6ce599f4876ba368fb05fdf32711f21a0b989911
jloiz <private@mail.com>
2026-08-29 15:45:28 +0100

    untestable code: untestable4, create a database connection in the tests

--- tdd-mooc-untestable-code/test/untestable4.test.mjs
+++ tdd-mooc-untestable-code/test/untestable4.test.mjs
@@ -1,6 +1,9 @@
 import { afterAll, afterEach, beforeAll, beforeEach, describe, test } from "vitest";
 import { PasswordService, PostgresUserDao, PostgresUser } from "../src/untestable4.mjs";
 import { execSync } from 'node:child_process'
+import pg from "pg"
+import { verify } from "@node-rs/argon2";
+import { expect } from "chai"
 
 describe("Untestable 4: enterprise application", () => {
   // let service;
@@ -13,16 +16,39 @@ describe("Untestable 4: enterprise application", () => {
   //   //PostgresUserDao.getInstance().close();
   // });
 
-  beforeAll(() => {
+  let db;
+
+  async function connectToDb(){
+    
+    const connInfo = {
+      host: '127.0.0.1',
+      port: '5433',
+      user: 'untestable',
+      password: 'secret',
+      database: 'postgres',
+    }
+    db = new pg.Pool(connInfo)
+    await verifyConnection(db)
+  }
+
+  async function verifyConnection(db)  {
+    let retries = 200
+    while(retries > 0) {
+    try{
+      await db.query("SELECT 1")
+      console.log("Postgres ready")
+      return
+    } catch (error) {
+      console.log("Failed to connect to database: ", error)
+      await new Promise(r => setTimeout(r, 250))
+      retries--
+    }
+  }
+  }
+
+  beforeAll( async () => {
     execSync("docker compose up -d")
-  })
-
-  afterAll(() => {
-    execSync("docker compose down")
-  })
-
-  test("docker container for tests starts", () => {
-
+    await connectToDb()
   })
 
   test("it can be constructed with a database connection", async () => {
