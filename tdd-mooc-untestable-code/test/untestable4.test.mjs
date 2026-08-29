69c878ee9bfb756f3e9917331a86e140a042f555
jloiz <private@mail.com>
2026-08-29 15:46:14 +0100

    untestable code: untestable4, formatting

--- tdd-mooc-untestable-code/test/untestable4.test.mjs
+++ tdd-mooc-untestable-code/test/untestable4.test.mjs
@@ -1,9 +1,9 @@
 import { afterAll, afterEach, beforeAll, beforeEach, describe, test } from "vitest";
 import { PasswordService, PostgresUserDao, PostgresUser } from "../src/untestable4.mjs";
-import { execSync } from 'node:child_process'
-import pg from "pg"
+import { execSync } from "node:child_process";
+import pg from "pg";
 import { verify } from "@node-rs/argon2";
-import { expect } from "chai"
+import { expect } from "chai";
 
 describe("Untestable 4: enterprise application", () => {
   // let service;
@@ -18,41 +18,40 @@ describe("Untestable 4: enterprise application", () => {
 
   let db;
 
-  async function connectToDb(){
-    
+  async function connectToDb() {
     const connInfo = {
-      host: '127.0.0.1',
-      port: '5433',
-      user: 'untestable',
-      password: 'secret',
-      database: 'postgres',
-    }
-    db = new pg.Pool(connInfo)
-    await verifyConnection(db)
+      host: "127.0.0.1",
+      port: "5433",
+      user: "untestable",
+      password: "secret",
+      database: "postgres",
+    };
+    db = new pg.Pool(connInfo);
+    await verifyConnection(db);
   }
 
-  async function verifyConnection(db)  {
-    let retries = 200
-    while(retries > 0) {
-    try{
-      await db.query("SELECT 1")
-      console.log("Postgres ready")
-      return
-    } catch (error) {
-      console.log("Failed to connect to database: ", error)
-      await new Promise(r => setTimeout(r, 250))
-      retries--
+  async function verifyConnection(db) {
+    let retries = 200;
+    while (retries > 0) {
+      try {
+        await db.query("SELECT 1");
+        console.log("Postgres ready");
+        return;
+      } catch (error) {
+        console.log("Failed to connect to database: ", error);
+        await new Promise((r) => setTimeout(r, 250));
+        retries--;
+      }
     }
   }
-  }
 
-  beforeAll( async () => {
-    execSync("docker compose up -d")
-    await connectToDb()
-  })
+  beforeAll(async () => {
+    execSync("docker compose up -d");
+    await connectToDb();
+  });
 
   test("it can be constructed with a database connection", async () => {
-    const pgUser = new PostgresUser(db)
-    expect(pgUser).to.exist
+    const pgUser = new PostgresUser(db);
+    expect(pgUser).to.exist;
   });
 });
