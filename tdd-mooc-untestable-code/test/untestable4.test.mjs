2a46c9bdad72f2b9389b66a9eefe32fe6c694633
jloiz <private@mail.com>
2026-08-29 14:29:31 +0100

    untestable code: untestable4, add docker prior to tests

--- tdd-mooc-untestable-code/test/untestable4.test.mjs
+++ tdd-mooc-untestable-code/test/untestable4.test.mjs
@@ -1,15 +1,29 @@
-import { afterEach, beforeEach, describe, test } from "vitest";
+import { afterAll, afterEach, beforeAll, beforeEach, describe, test } from "vitest";
 import { PasswordService, PostgresUserDao, PostgresUser } from "../src/untestable4.mjs";
+import { execSync } from 'node:child_process'
 
 describe("Untestable 4: enterprise application", () => {
-  let service;
-  beforeEach(() => {
-    service = new PasswordService();
-  });
+  // let service;
+  // beforeEach(() => {
+  //   service = new PasswordService();
 
-  afterEach(() => {
-    //PostgresUserDao.getInstance().close();
-  });
+  // });
+
+  // afterEach(() => {
+  //   //PostgresUserDao.getInstance().close();
+  // });
+
+  beforeAll(() => {
+    execSync("docker compose up -d")
+  })
+
+  afterAll(() => {
+    execSync("docker compose down")
+  })
+
+  test("docker container for tests starts", () => {
+
+  })
 
   test("it can be constructed with a database connection", async () => {
     const pgUser = new PostgresUser(db)
